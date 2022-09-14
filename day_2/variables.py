c35323489e3d932b0c1f5b89a0d31359ec4f9cf1
Aelwin <mithrandir01@hotmail.com>
2022-09-14 08:11:39 +0200

    pruebas con variables

---
+++ day_2/variables.py
@@ -0,0 +1,29 @@
+#Day 2: 30 Days of python programming
+from math import remainder
+
+
+first_name = "Juan"
+last_name = "López"
+full_name = first_name + last_name
+year = 2022
+is_married = False
+is_light_on = True
+
+#Multiline declare variables
+country, city, age, is_true = "España", "Vitoria", 38, True
+
+print("El tipo de first_name es", type(first_name))
+print("El tipo de age es", type(age))
+print("El tipo de is_married es", type(is_married))
+
+print("Mi nombre tiene una longitud de", len(first_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_one * num_two
+division = num_one / num_two
+_remainder = num_two % num_one
+exp = num_one**num_two
+floor = num_one//num_two
